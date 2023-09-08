95ff0c1be9ae910d28c62675208469efb8486c3d
Stevy Endaman <stevyabessolo@gmail.com>
2023-09-09 00:54:19 +0100

    add middleware

--- nftMarket/routes/web.php
+++ nftMarket/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -14,13 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
- // Route d'accueil, qui liste tous les NFT
- Route::get('/', function () {
-    return view('nfts/nfts');
+
+// route user non authentifié
+Route::middleware('guest')->group(function () {
+    // Route d'accueil, qui liste tous les NFT
+    Route::get('/', function () {
+        return view('nfts/nfts');
+    });
 });
 
+// route authentifi 
+Route::middleware('auth')->group(function () {
+    Route::middleware('role:admin')->group(function () {
+        // route admin authentifié
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
+    });
 
+    Route::middleware('role:user')->group(function () {
+        // route user authentifié
+    });
+});
 
 Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
